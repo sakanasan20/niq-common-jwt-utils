549a1e0ad7c4eab09f5cb76c94fc49aa952cd718
Nick Chen <nick.chen@cmp.com.tw>
2023-12-14 10:34:04 +0800

    Initial commit

---
+++ src/main/java/tw/niq/common/NiqCommonJwtUtilsApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiqCommonJwtUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiqCommonJwtUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/niq/common/NiqCommonJwtUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.niq.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiqCommonJwtUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
